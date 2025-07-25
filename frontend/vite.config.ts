0d1c4cc1ce277b4bd7ebc025dcfd29344f2acc7c
Konstantin Simusev <simussev.k@icloud.com>
2025-07-25 14:42:48 +0500

    fix_010: deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Origin": "films.nomorepartiessbs.ru",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
